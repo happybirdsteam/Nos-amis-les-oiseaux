ed60b0693153cadb4de82931af30fb00d7c3e874
Stan <stanislas.schoirfer@ensa-nancy.fr>
2016-10-04 18:15:01 +0200

    add DateTime
    TO DO better view

--- src/AppBundle/Form/ObservationType.php
+++ src/AppBundle/Form/ObservationType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -29,6 +30,12 @@ class ObservationType extends AbstractType
             ))
             ->add('comment', TextareaType::class, array(
                 'required' => false
+            ))
+            ->add('date',  DateTimeType::class, array(
+                'placeholder' => array(
+                    'year' => 'Year', 'month' => 'Month', 'day' => 'Day',
+                    'hour' => 'Hour', 'minute' => 'Minute', 'second' => 'Second',
+                )
             ))
             ->add('bird', TextType::class)
             ->add('image', FileType::class, array(
