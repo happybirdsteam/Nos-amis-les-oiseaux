c007ad9952dfdc18e2cb837e2a0181786544277c
emousse <etienne.mousse@gmail.com>
2016-10-10 09:39:43 +0200

    fixture

--- src/AppBundle/DataFixtures/ORM/LoadObservationData.php
+++ src/AppBundle/DataFixtures/ORM/LoadObservationData.php
@@ -16,8 +16,8 @@ class LoadObservationData implements FixtureInterface
         $lat = 46.990896;
         $lng = 3.162845;
         for ($i = 0; $i<100; $i++){
-            $latRandom = $lat + (rand(1,9) / (rand(1,2)*10));
-            $lngRandom = $lng + (rand(1,9) / (rand(1,2)*10));
+            $latRandom = rand(0,1) ? $lat + (rand(1,9) / (rand(1,2)*10)) : $lat + (rand(1,9) / (rand(1,2)*10));
+            $lngRandom = rand(0,1) ? $lng + (rand(1,9) / (rand(1,2)*10)) : $lng + (rand(1,9) / (rand(1,2)*10));
             $observation = new Observation();
             $observation->setLat($latRandom);
             $observation->setLng($lngRandom);
